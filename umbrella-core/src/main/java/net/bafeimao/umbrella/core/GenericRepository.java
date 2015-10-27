064810a36d99ec11258b82961068bd241b1b61ad
bafeimao <29283212@qq.com>
2015-10-27 09:43:30 +0800

    add files

---
+++ umbrella-core/src/main/java/net/bafeimao/umbrella/core/GenericRepository.java
@@ -0,0 +1,7 @@
+package net.bafeimao.umbrella.core;
+
+/**
+ * Created by Administrator on 2015/10/27.
+ */
+public class GenericRepository {
+}
---
+++ umbrella-core/src/main/java/net/bafeimao/umbrella/core/util/JsonUtil.java
@@ -0,0 +1,7 @@
+package net.bafeimao.umbrella.core.util;
+
+/**
+ * Created by Administrator on 2015/10/27.
+ */
+public class JsonUtil {
+}
