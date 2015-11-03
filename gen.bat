ff9703d168810a3065206ac9b39f695e5487982f
bafeimao <29283212@qq.com>
2015-11-03 18:43:25 +0800

    add files

---
+++ gen.bat
@@ -0,0 +1,3 @@
+@echo off
+
+protoc --proto_path="D:\IdeaProjects\umbrella\props" --java_output="D:\IdeaProjects\umbrella\umbrella-servers\src\main\java\net\bafeimao\umbrella\servers\world\generated\" 
